ad74d344fcc4b5298a187f32e5fa79f36619ec68
devRabbani <dev.golamrabbani@gmail.com>
2023-02-01 20:18:03 +0530

    🎉 BEGIN: Start the project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import HeroSection from './components/HeroSection'
+
+function App() {
+  return (
+    <main>
+      <HeroSection />
+      <HeroSection />
+      <HeroSection />
+      <HeroSection />
+      <HeroSection />
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/HeroSection/index.jsx
@@ -0,0 +1,22 @@
+export default function HeroSection() {
+  return (
+    <section className="heroSection">
+      <div className="wrapper hero">
+        <h1>An Online Student Application Form</h1>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam fugit
+          repudiandae placeat ex numquam nam voluptas, est cupiditate vel atque
+          vitae iusto quo at aut similique magnam odio ipsam eum, fugiat iure
+          officiis provident? Hic unde consequatur voluptatem nisi vero! Aut,
+          hic perspiciatis. Animi, voluptatem! Minus accusantium consequuntur
+          architecto quod libero laudantium aperiam iste fugiat necessitatibus,
+          mollitia impedit quia doloremque saepe dicta excepturi sapiente sed
+          animi quo veritatis perspiciatis nobis debitis neque quasi fuga. Minus
+          corporis neque debitis adipisci accusantium aut architecto animi sed
+          laudantium quod dignissimos nobis veritatis fugit, corrupti officia
+          voluptates nihil quidem unde iste, minima perferendis ab.
+        </p>
+      </div>
+    </section>
+  )
+}
