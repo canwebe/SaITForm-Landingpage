c9ba5a9a2a0ac156ad37f3fb5bb84cf80ca84aef
devRabbani <dev.golamrabbani@gmail.com>
2023-02-03 01:31:32 +0530

    👌 IMPROVE: Completed contact section and improved demo section

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,9 @@ function App() {
         <ProblemSection />
         <SolutionSection />
       </section>
-      <DemoApp />
+      <section>
+        <DemoApp />
+      </section>
     </main>
   )
 }
--- src/components/DemoApp/index.jsx
+++ src/components/DemoApp/index.jsx
@@ -1,24 +1,38 @@
 export default function DemoApp() {
   return (
-    <section className="demoapp-wrapper">
-      <div className="laptop">
-        <img src="/public/laptop.png" alt="laptop mockup" />
+    <>
+      <div className="demoapp-wrapper">
+        <div className="laptop">
+          <img src="/public/laptop.png" alt="laptop mockup" />
+        </div>
+        <div className="bg-blue">
+          <div className="demoapp-info wrapper">
+            <h1>Here is the full demo of the App</h1>
+            <p>
+              It is comes with Admin panel Admin panel connected to colleges
+              Only activated and authenticated machines can use this application
+              It can Show list of students
+            </p>
+          </div>
+          <div className="contact-info wrapper">
+            <h3>
+              If you want this service or other CWBEdu services to your
+              institution
+            </h3>
+            <button className="btn-contact">Contact Us</button>
+            <p>
+              Designed and Developed By{' '}
+              <a
+                href="https://canwebe.tech"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                CanWeBe!
+              </a>
+            </p>
+          </div>
+        </div>
       </div>
-      <div className="demoapp-info">
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio eius
-          voluptatum eos consequatur necessitatibus, ipsum natus fugit
-          blanditiis tempore, doloremque cumque unde sint molestiae placeat,
-          nobis explicabo vitae atque debitis. Lorem ipsum, dolor sit amet
-          consectetur adipisicing elit. Odio eius voluptatum eos consequatur
-          necessitatibus, ipsum natus fugit blanditiis tempore, doloremque
-          cumque unde sint molestiae placeat, nobis explicabo vitae atque
-          debitis. Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-          Odio eius voluptatum eos consequatur necessitatibus, ipsum natus fugit
-          blanditiis tempore, doloremque cumque unde sint molestiae placeat,
-          nobis explicabo vitae atque debitis.
-        </p>
-      </div>
-    </section>
+    </>
   )
 }
